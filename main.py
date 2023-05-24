d191f4810934f8e9eba2fd228419d979d18c0c0b
dalimovali <dalimovali07@gmail.com>
2023-05-24 18:52:12 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,523 @@
+# a = int(input("ведите первое число "))
+# b = int(input("ведите второе число "))
+# print("результат -", a + b)
+
+
+# a, b, d= 100, 200, 300
+# print(a + b)
+
+# name = "begimay"
+# print(type(name))
+
+
+
+
+# a = 3
+# b = 5
+# print(a + b) 
+
+
+# # a = 9 
+# b = 7 
+# print(a - b)
+
+# a = 4 
+# b = 1 
+# print(a * b)
+
+# a = 5
+# b = 2
+# print(a / b)
+
+# a = 5
+# b = 2 
+# print(a // b)
+
+# a = 7 
+# b = 2 
+# print(a + b)
+
+# a = 25
+# b = 5 
+# print(a % b)
+
+# a = 2.5 
+# b = 4.2 
+# print(a + b)
+
+# a = 8.3
+# b = 0.7 
+# print(a - b)
+
+# a = 0 
+# b =9.2 
+# print(a * b)
+
+# a = 7.2 
+# b = 8 
+# print(a / b)
+
+# a = 3.2 
+# b = 8
+# print(a // b)
+
+# a = 14.2 
+# b = 4.8 
+# print(a + b)
+
+# a = 2.7 
+# b = 0.3 
+# print(a % b)
+
+
+# a = 10 
+# b = 8
+# c = 10
+# d = 9
+# e = 12
+# print(a-e**(b/d))%c
+
+
+# a = 1350
+# b = 2830
+# print(a < b)
+
+# a = 450
+# b = 750
+# print(a > b)
+
+
+# name = input("ведите имя: ")
+# print("Здравствуйте,", name.lower())
+
+# name = input("ведите имя: ")
+# print("Здравствуйте,", name.upper())
+
+# name = input("ведите имя: ")
+# print("Здравствуйте,", name.title())
+
+# name = ("ведите имя:")
+# age = input("ваш возраст: ")
+# film = input("ведите ваш фильм") 
+
+# print("ведите имя - ", name, "ваш возвраст -", age, "ведите ваш фильм - ", film)
+# print(f"ваша имя - {name} ваш возраст - {age} ведите ваш фильм  - {film}" )
+
+# name = 'husanbay'
+# lastname = 'haydarovhusniddinovich'
+
+# # print(name[2])
+# # print(lastname[9::5])
+
+
+# print(lastname.endswith("h"))
+# print(lastname.startswith("a"))
+# print(lastname.split(" "))
+# print(lastname.count("i"))
+# print(len(lastname))
+# print(lastname.center(50))
+
+
+# place = 'Madagaskar'
+
+# print(place.endswith("g"))
+# print(place.startswith("a"))
+# print(place.split(" "))
+# print(place.count("M"))
+# print(len(place))
+# print(place.center(50))
+# print(place[0:5])
+
+
+
+
+
+
+
+# num1 = 4
+# num2 = 1
+
+# if num1 > num2:
+#     print(f"{num1} большет {num2}")
+# elif num1 == num2:
+#     print(f"{num1} равен {num2}")
+# else:
+#     print(f"{num1} меньше {num2}")
+
+
+
+
+
+
+
+# num = int(input("ведите число"))
+# if num == 0:
+#     print(f"{num} нейтральный") 
+# elif num % 2 == 0:
+#     print(f"{num} чотный")
+# else:
+#     print(f"{num} нечетный")
+
+
+
+
+
+
+
+# a = 2  
+# b = 3  
+# if a > b:
+#     print(f"{a} больше {b}")
+# elif a == b:
+#     print(f"{a} равен {b}")
+# else:
+#     print(f"{a} меньше {b}")
+
+
+
+
+
+
+ 
+# num = int(input("ведите число: "))
+# if num >= 0 and num <= 21:
+#     print(f"{num}разрешен")
+# elif num == 57 and num <= 100:
+#     print(f"{num} разрешен")
+# else:
+#     print(f"{num} не разрешен")
+
+
+
+
+
+
+
+
+
+
+# a = 45
+# b = 6
+
+# if a % b == 6:
+#    print("yes")
+# else:
+#    print("no")
+
+
+
+
+# x = 13.
+
+# age = int(input(172))
+# if age >= 18:
+#   print("Совершеннолетний")
+# elif age >= 0 and age <= 4:
+#   print("Ребенок")
+# else:
+#   print("Несовершеннолетний")
+
+
+
+
+
+# num = int(input("ведите число: "))
+# if num >= 0 and num <= 21:
+#   print(f"{num} разрешен")
+# elif num == 57 and num <= 100:
+#   print(f"{num} разрешен")  
+# else:
+#   print(f"{num} не разрешен")
+
+
+
+
+# um = int(input("Введите число: "))
+# if num % 3 == 0 and num % 4 == 3:
+#    print(f"{num} четный")
+# elif num ==  1 and num ==  19:
+#    print(f"{num} четный")
+# else:
+#     print(f"{num} нечетный")
+
+
+
+
+
+
+
+
+# num = [1,2, (1,2,3), "house", 35, 1,1]
+# print(num.count(1))
+
+# num1 = [3,5,2, 9, 1, 4]
+# num1.sort()
+# print(num1)
+
+# names = ['Aigerim', 'Jylduz', 'Bekmmat', 'Nurbek']
+# names[0] = "Akylai"
+# print(names)
+
+
+# number = [1, 2, [1, 2; 3], "house", 3,5]
+# new = list (number)
+# print(type(number))
+# print(new)
+# new[0] = 2
+# print(new)
+# number = tuple(new)
+# print(number)
+
+
+# lst = [(1,2), (3,4), (5,6), (7,8), (9,10)]
+# print (lst[3])
+
+
+# a = (1, 2, 3)
+# print(a[0])
+# print(a[1])
+# print(a[2])
+
+
+
+
+
+
+
+# # lst = [(1,2), (3,4), (5,6), (7,8), (9,10)]
+# print (lst[3])
+
+
+
+# a = (1, 2, 3)
+# print(a[0])
+# print(a[2])
+# print(a[2])
+
+# names = ['ali', 3.5,  1j, 5, [3,7,5,2], ("one", "two")]
+# print (names[4])
+
+# num = [1, (1,2,3), "house", 3.5, 1,1,1]
+# name = ("alike", "Jack", "Emiliy")
+# num.append(35)
+# num.append("one")
+# num.remove(1)
+# num.remove((1,2,3))
+# num.pop(0)
+# # num.pop(1)
+# # num.pop(0)
+# num.reverse()
+# num.insert(0, 10)
+# print(num)
+# # print(num[-2])
+
+
+# names =["Husanbay", "Bekbolot", "Emil", "ali"]
+# print(names.count("Bekbolot"))
+# names.remove("ali")
+# names.pop(2)
+# print(names)
+
+# mydata = []
+# mydata.append("ali")
+# mydata.append(2007)
+# mydata.append("python")
+# mydata.print("mydata")
+
+# cehetnyi = []
+# necehetnyi = []
+
+# for i in range(21):
+#     if i % 2 == 0:
+#       cehetnyi.append(i)
+#     else:necehetnyi.append(i)
+
+# prind(f" (Чотный числа  ("cehetny"))
+
+
+
+
+# a = 17
+# b = 3
+# c = 5
+
+# while a < 23:
+#     print(b,">", c)
+
+
+
+# string = "hello"
+# # print(len(string))
+# # цикл for
+# for i in string:
+#     print(i)
+
+# # цикл while
+# i = 0 
+# while i < len(string):
+#     print(string[i])
+#     i += 1
+
+
+
+
+# count = 0 
+# count1 = 0
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         count += 1
+#     else:
+#         count1 += 1
+# print(count)
+# print(count1)
+
+
+
+
+# my_list = [1, 2]
+# print(len(my_list))
+# # цикл for
+# for i in my_list:
+#     print(i)
+
+# # цикл while
+# i = 0
+# while i < len(my_list):
+#     print(my_list[i])
+#     i += 1
+    
+
+
+# password = 123456
+# count = 3
+# loged_in = True
+
+# while count > 0:
+#     user_input = int(input("Введите пароль"))
+
+#     if user_input == password:
+#         leged_in = True
+#         break
+#     else:
+#         count -= 1
+#         print("Неверный парль. Повторите попытку")
+
+# if leged_in:
+#     print("Выход выполнен")
+# else:
+#     print("увас не осталось попыток")
+
+
+
+
+# sum = 0 
+# num = int(input("Введите число :"))
+# while num != 0:
+#     sum += num
+#     num = int(input("Введите число :"))
+
+# print("Сумма введеных чисел - ", sum)
+
+
+
+# list = [28, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+# for i in:
+#     if i:
+#         break
+#     print(i)
+
+
+
+
+
+# for i in range(1, 6):
+#     print(f"строка {i} 0")
+
+
+
+# for i in range(1, 101):
+#     print(f"чисел {i} 0")
+# sum = 0
+# for i in range(1, 101):
+#     sum += i
+# print(sum)
+
+
+
+
+# a = 8
+# b = 0
+# c = 8
+# if a == b or a == c or b == c:
+#     print('Yes')
+# else:
+#     print("Error")
+
+
+
+
+
+# for i in range(1, 498):
+#     print(f"чисел {i} ")
+#     sum = 0
+#     sum += i
+# print(sum)
+
+
+
+
+# list1 = [3, 5, 2, 1, 4]
+# list2 = [4, 2, 6, 8, 1]
+
+# a = list1 = list2
+# b = set(a)
+# print(b)
+# c = list(b)
+# print(c)
+
+
+
+
+
+# lst = []
+# for i in range(16):
+#     lst.append(i)
+
+# max_lst = max(lst)
+# min_lst = min(lst)
+# print(max_lst + min_lst)
+
+
+
+
+# month = int(input("Введите номер месяца: "))
+# if month == 12 or month == 1 or month == 2:
+#     print("Сезон года: Зима")
+# elif month >= 3 and month <= 5:
+#     print("Сезон года: Весна")
+# elif month >= 6 and month <= 8:
+#     print("Сезон года: Лето")
+# elif month >= 9 and month <= 11:
+#     print("Сезон года: Осень")
+# else:
+#     print("Некорректный номер месяца")
+
+
+
+# a = ['a', 'e', 'i', 'o', 'u']
+# sstr = input("Введите слова")
+# new = sstr.split()
+
+# for i in new:
+#     if i[0].lower()in a:
+#         print(i)
+
+
+
+# r = int(input("Введите радиус"))
+# res = 2 * 3.14 * r
+# print(f"длина окружуности - {res}")
+
+
+
+
