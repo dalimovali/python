5f62ec1b1d5cc0eca364c11eb5025275ba1e3207
dalimovali <dalimovali07@gmail.com>
2023-05-26 19:58:06 +0600

    file

--- main.py
+++ main.py
@@ -511,3 +511,197 @@
 
 
 
+# a = [True, True, True, True]
+# print(any(a))
+# print(all(a))
+
+
+# num = 3.14485068456
+# print(round(num, 3))
+
+
+
+# loan = float (input("ведите сумму займа"))
+# protcent = 3.47
+# total =loan * protcent / 100
+# if loan >=50000:
+#     print(round(total, 2))
+# else:
+#     print("ведите сумму больше 50000")
+
+# setm =set()
+# for i in range (5):
+    # i = int(input("ведите 5 чисел: "))  
+    # setm. add(i)
+# print(min(setm))
+
+# try:
+#     a = int (input())
+#     b = int (input())
+#     print(a / b)
+# except ZeroDivisionError:
+#     print("Нельзя делить на")
+
+
+# values = ['a', 'b', (4, 5), 'c']
+
+# convertibility = []
+# for item in values:
+#     try:
+#         set(item)
+#         convertibility.append(True)
+#     except TypeError:
+#         convertibility.append(False)
+
+# can_convert = all(convertibility)
+# print("Можно ли конвертировать values в SET?", can_convert)
+
+
+# user_input = input("Введите Python функцию: ")
+
+# try:
+#     result = eval(user_input)
+#     print("Результат выполнения функции", result)
+# except NameError:
+#     print("В Python такой фунции нет!")
+
+
+
+# try:
+    # my_list = [1, 2, 3]
+    # print(my_list[9])
+# except IndexError:         
+    # print("такого индекса нету")
+
+
+# try:
+    # if 5 = 6:
+    #  print(5==6)
+# except IndexError:          
+    # print("Возникла синтасическая ошибка")
+
+
+# while True:
+#     try:
+#         num1 = int(input())
+#         a = input()
+#         num2 = int(input())
+#         if a == '+':
+#             print(num1 + num2)
+#         elif a == "-":
+#             print(num1 - num2)
+#         elif a == '/':
+#             try: 
+#                 print(num1 / num2)
+#             except ZeroDivisionError:
+#                 print("Нельзя делить на ноль!")
+#             finally:
+#                 print('YES')
+#         elif a == '*':
+#             print(num1 * num2)
+#         elif a == '':
+#             print(num1**num2)
+#         else:
+#             print("Error")
+#     except ValueError :
+#         print("Введите число!")
+
+# file = open("new.txt", "w")
+# file.write("hello, world\n")
+# file.write("111.sty1e")
+# file.close()
+
+
+
+
+
+
+
+
+
+
+
+# file = open("new.txt", "r", encoding="utf-8")
+# # file.write("hello, world\n")
+# # file.write("111.sty1e")
+# s = file.read()              1234567890-=
+# print(s)
+# file.close()
+
+
+
+
+
+
+
+# login = input("Введите логин: ")
+# password = int(input("Введите пароль: "))
+# with open("userx.txt", "a", encoding="utf-8") as file:
+#     file.write(f"Ваш логин-{login}, ваш пароль{password}\n")
+
+
+
+# try:
+#     with open("userx.txt", "r") as file:
+#         s =file.read()
+#         print (s)
+#         file.close()
+# except FloatingPointError:
+#     print("файл не найден")
+
+
+# file_name = "new.txt"
+
+# try:
+#     with open (file_name, "r") as file:
+#         text = file.read()
+
+#         if "w" in text:
+#             print("да в, тексте есть w")
+#         else:
+#             print("нет в, тексте нет w")
+# except FileExistsError:
+#     print("файл не найден")
+
+
+
+
+
+
+# file_name = "Lesson.txt"
+
+# with open(file_name, "w") as file:
+#     text = file.write("eifh oif hshfteh trrttt tggggg")
+
+# with open(file_name, "r") as file: 
+#     text = file.read()
+# t_words = []
+# for word in text.split(): 
+#     if "t" in word.lower():
+#         t_words.append(word)
+
+# for word in t_words:
+#     print(word)
+
+
+numbers = [ 19, 3, 24, 7]
+file_open = "lesson.txt"
+
+with open(file_open, "w") as file:
+    for number in numbers:
+        file.write(str(number) + "\n")
+
+max_number = float("-inf")
+min_number = float("inf")
+
+with open(file_open, "r") as file:
+    for line in file:
+        number = int(line.strip())
+        max_number = max(max_number, number)
+        min_number = min(min_number, number)
+
+output_file = "resul.txt"
+
+with open(output_file, "w") as file:
+    file.write("Максимальное число: " + str(max_number) + "\n")
+    file.write("Минимальное число: " + str(min_number) + "\n")
